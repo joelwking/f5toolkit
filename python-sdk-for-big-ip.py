9121c4e9e813d291dc7c9604701b7a9038c80108
Joel W. King <joel.king@wwt.com>
2016-10-04 11:30:30 -0400

    Update python-sdk-for-big-ip.py

--- python-sdk-for-big-ip.py
+++ python-sdk-for-big-ip.py
@@ -4,6 +4,9 @@
 #            https://devcentral.f5.com/articles/f5-friday-python-sdk-for-big-ip-18233
 #
 import sys
+import time
+import requests
+requests.packages.urllib3.disable_warnings()
 from f5.bigip import BigIP
 
 # Connect to the BigIP
@@ -15,8 +18,8 @@ bigip = BigIP(sys.argv[1], sys.argv[2], sys.argv[3])
 pools = bigip.ltm.pools.get_collection()
 for pool in pools:
     print pool.name
-    for member in pool.members:
-        print member.name
+    # for member in pool.members:
+        # print member.name
 
 # Create a new pool on the BigIP
 mypool = bigip.ltm.pools.pool.create(name='mypool', partition='Common')
@@ -25,8 +28,12 @@ mypool = bigip.ltm.pools.pool.create(name='mypool', partition='Common')
 pool_a = bigip.ltm.pools.pool.load(name='mypool', partition='Common')
 pool_a.description = "New description"
 pool_a.update()
+#
+# Sleep so we can see the new pool and description in the GUI
+time.sleep(20)
 
+#
 # Delete a pool if it exists
 if bigip.ltm.pools.pool.exists(name='mypool', partition='Common'):
-    pool_b = bigip.ltm.pools.pool.load(name='oldpool', partition='Common')
+    pool_b = bigip.ltm.pools.pool.load(name='mypool', partition='Common')
     pool_b.delete()
